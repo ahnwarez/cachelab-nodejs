08d1ea9aa3dec8f207596ddcb2ca0a85d5a6835d
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-09-06 05:45:30 +1200

    read argv

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+import { initCache, access } from "./cache.js";
+
 const argc = process.argv.length;
 
-if (argc < 6) {
+if (argc < 9) {
   console.log(`Usage:
+-h help
 -s <Set Index>
 -E <Cache lines>
 -b <Block offest>
--f <file name>`);
+<file name>`);
 }
 
-let cache = initCache({ s: 4, E: 1, b: 4 });
+const s = Number(process.argv[3]);
+const E = Number(process.argv[5]);
+const b = Number(process.argv[7]);
+const f = process.argv[8];
+
+let cache = initCache({ s, E, b });
 let hits = 0;
 let misses = 0;
 let evictions = 0;
@@ -22,3 +30,5 @@ cache = newCache;
 if (outcome.hit) hits++;
 if (outcome.miss) misses++;
 if (outcome.eviction) evictions++;
+
+console.log(`hits: ${hits} misses: ${misses} evictions: ${evictions}`);
